1b426439eac975139cf3ec82a75ebdaf984ab7c7
Justin Wozniak <woz@anl.gov>
2021-07-14 13:39:09 -0500

    Fix file names; test works

--- test/test-py-1.sh
+++ test/test-py-1.sh
@@ -9,7 +9,7 @@ EQ_SQL=$( readlink --canonicalize $THIS/.. )
 export PYTHONPATH=$EQ_SQL/db:$EQ_SQL/python
 
 which python3
-python3 $EQ_SQL/test/py-test-1-me.py &
-python3 $EQ_SQL/test/py-test-1-wf.py
+python3 $EQ_SQL/test/test-py-1-me.py &
+python3 $EQ_SQL/test/test-py-1-wf.py
 
 wait
