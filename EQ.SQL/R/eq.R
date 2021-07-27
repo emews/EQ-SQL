ccd32830e348251977231bd1047a5e451d634975
Justin Wozniak <woz@anl.gov>
2021-07-27 11:32:22 -0500

    Basic insert works

--- EQ.SQL/R/eq.R
+++ EQ.SQL/R/eq.R
@@ -1,14 +1,79 @@
 
 #' @export
 eq.init <- function() {
-    print("init")
+  # print("init")
+  success <- TRUE  # assume success
+  DB_PORT = Sys.getenv("DB_PORT")
+  if (nchar(DB_PORT) == 0) {
+    success <- FALSE
+    print("Set environment variable DB_PORT!")
+  }
+  DB_NAME = Sys.getenv("DB_NAME")
+  if (nchar(DB_NAME) == 0) {
+    success <- FALSE
+    print("Set environment variable DB_NAME!")
+  }
+  if (! success) {
+    return(FALSE)
+  }
+  # cat(DB_PORT, "\n")
+  # cat(DB_NAME, "\n")
+  # print(DB_PORT, DB_NAME)
+
+  conn <<- DBI::dbConnect(RPostgres::Postgres(),
+                         dbname = DB_NAME, host = "localhost", port = DB_PORT)
+  # print(conn)
+  # print("OK")
+  TRUE
 }
 
 #' @export
 eq.OUT_put <- function(msg) {
-    printf("OUT_put(%s)\n", msg)
+  printf("OUT_put(%s)\n", msg)
+  # print(conn)
+  queue_push("emews_queue_OUT", Q(msg))
+}
+
+queue_push <- function(table, value) {
+  cat("\n")
+  rs <- dbGetQuery(conn, "select nextval('emews_id_generator');")
+  cat("rs: _", rs[1,1], "_\n")
+  # Convert SQL integer64 to R integer:
+  id <- as.integer(rs[1,1])
+  # print(id)
+  # cat("cat id ", id, "\n")
+  # cat("push: value", value, "\n")
+  # printf("rs: %f\n", x)
+  SQL.insert(table, list("eq_id", "json"), list(id, value))
+}
+
+SQL.insert <- function(table, names, values) {
+  print("insert")
+  n <- SQL.tuple(names)
+  cat("SQL.insert: values: ", paste(values), "\n")
+  v <- SQL.tuple(values)
+  cat("SQL.insert: v: ", paste(v), "\n")
+  cmd <- sprintf("insert into %s %s values %s;", table, n, v)
+  rs <- SQL.execute(cmd)
+  cat("result ", rs, "\n")
+}
+
+SQL.tuple <- function(L) {
+  cat("SQL.tuple: ", paste(L, collapse=","), "\n")
+  sprintf("(%s)", paste(L, collapse=","))
+}
+
+SQL.execute <- function(cmd) {
+  cat("SQL.execute: ", cmd, "\n")
+  dbExecute(conn, cmd)
+}
+
+Q <- function(s) {
+  # Just quote the given string
+  paste0("'", s, "'")
 }
 
 # Local Variables:
+# ess-indent-level: 2
 # eval: (flycheck-mode)
 # End:
--- test/test-R-1-me.R
+++ test/test-R-1-me.R
@@ -5,12 +5,16 @@ library(EQ.SQL)
 
 print("R TEST 1 ME: START")
 
-eq.init()
-
-for (i in seq(3)) {
-  eq.OUT_put(sprintf("message:%i", i))
+if (! eq.init()) {
+  quit(status=1)
 }
-eq.OUT_put("EQ_FINAL")
+
+## for (i in seq(3)) {
+##   eq.OUT_put(sprintf("message:%i", i))
+## }
+## eq.OUT_put("EQ_FINAL")
+
+eq.OUT_put(sprintf("message:%i", 42))
 
 ## print("PY TEST 1 ME: STOP")
 
