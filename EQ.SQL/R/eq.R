2492466811ff68fb5db33d8b151e8568f05f0938
Justin Wozniak <woz@anl.gov>
2021-07-27 14:28:39 -0500

    Add OUT_get()

--- EQ.SQL/R/eq.R
+++ EQ.SQL/R/eq.R
@@ -34,6 +34,19 @@ eq.OUT_put <- function(msg) {
   queue_push("emews_queue_OUT", Q(msg))
 }
 
+#' @export
+eq.OUT_get <- function(delay, timeout) {
+
+  result <- queue_pop("emews_queue_OUT", delay, timeout)
+
+  if (result != FALSE) {
+    result
+  } else {
+    print("eq.R:OUT_get(): popped nothing: abort!")
+    "EQ_ABORT"
+  }
+}
+
 queue_push <- function(table, value) {
   cat("\n")
   rs <- dbGetQuery(conn, "select nextval('emews_id_generator');")
@@ -45,6 +58,54 @@ queue_push <- function(table, value) {
   # cat("push: value", value, "\n")
   # printf("rs: %f\n", x)
   SQL.insert(table, list("eq_id", "json"), list(id, value))
+}
+
+
+sql_pop_q <- function(table) {
+  # Generate code for a queue pop from given table
+  #   From: https://www.2ndquadrant.com/en/blog/what-is-select-skip-locked-for-in-postgresql-9-5
+  template <- "
+    DELETE FROM %s
+    WHERE eq_id = (
+    SELECT eq_id
+    FROM %s
+    ORDER BY eq_id
+    FOR UPDATE SKIP LOCKED
+    LIMIT 1
+    )
+    RETURNING *;
+    "
+  sprintf(template, table, table)
+}
+
+queue_pop <- function(table, delay, timeout) {
+  sql_pop <- sql_pop_q(table)
+  start  <- Sys.time()
+  success <- FALSE
+  repeat {
+    if (Sys.time() - start > timeout) {
+      break  # timeout
+    }
+    delay <- delay * runif(1) * 2
+    Sys.sleep(delay)
+    rs <- dbSendQuery(conn, sql_pop)
+    df <- dbFetch(rs)
+    msg <- df[1,2]
+    count <- nrow(df)
+    dbClearResult(rs)
+    printf("%0.1f queue_pop(%s): '%i'\n", Sys.time(), table, count)
+    if (count > 0) {
+      success <- TRUE
+      break
+    }
+    delay <- delay * 2
+  }
+  if (! success) {
+    printf("queue_pop(%s): TIME OUT\n", table)
+    FALSE
+  } else {
+    msg
+  }
 }
 
 SQL.insert <- function(table, names, values) {
