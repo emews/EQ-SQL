f56722c538a46bdc8cb65feb6c82dbaaf3705a3e
Justin Wozniak <woz@anl.gov>
2021-10-07 16:45:51 -0500

    Better wait

--- test/test-py-1.sh
+++ test/test-py-1.sh
@@ -1,15 +1,17 @@
 #!/bin/zsh -f
 set -eu
 
-# TEST 1
+# TEST PY 1 SH
 
 THIS=$(   readlink --canonicalize $( dirname $0 ) )
 EQ_SQL=$( readlink --canonicalize $THIS/.. )
 
 export PYTHONPATH=$EQ_SQL/db:$EQ_SQL/python
 
+source $EQ_SQL/db/db-settings.sh
+
 which python3
 python3 $EQ_SQL/test/test-py-1-me.py &
+JOB=${!}
 python3 $EQ_SQL/test/test-py-1-wf.py
-
-wait
+wait ${JOB}
