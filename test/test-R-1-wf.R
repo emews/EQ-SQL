7497f6bc69feb78816624fcd2589ccd2e4c5f4c0
Justin Wozniak <woz@anl.gov>
2021-10-07 17:18:54 -0500

    Add emacs settings

--- test/test-R-1-wf.R
+++ test/test-R-1-wf.R
@@ -14,3 +14,9 @@ msg <- eq.OUT_get(eq_type=0, delay=1, timeout=3)
 cat("msg: ", msg, "\n")
 
 print("R TEST 1 WF: STOP")
+
+# Local Variables:
+# mode: R;
+# eval: (setq ess-default-style 'DEFAULT)
+# eval: (setq ess-indent-level 2)
+# End:
