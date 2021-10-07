79570d5d1b536e4f6b4772de15262776423261ca
Justin Wozniak <woz@anl.gov>
2021-10-07 16:46:25 -0500

    Read db-settings first

--- test/test-py-2.sh
+++ test/test-py-2.sh
@@ -8,6 +8,8 @@ EQ_SQL=$( readlink --canonicalize $THIS/.. )
 
 export PYTHONPATH=$EQ_SQL/db:$EQ_SQL/python
 
+source $EQ_SQL/db/db-settings.sh
+
 which python3
 python3 $EQ_SQL/test/test-py-2-me.py &
 python3 $EQ_SQL/test/test-py-2-wf.py
