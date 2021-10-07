b34e4ebac32ef351a9b24b0b8d93079b7a35b9f4
Justin Wozniak <woz@anl.gov>
2021-10-07 17:13:03 -0500

    Support eq_type

--- swift/EQ.swift
+++ swift/EQ.swift
@@ -19,23 +19,38 @@ string code_get = """
 import eq
 eq.init()
 try:
-    result = eq.OUT_get()
+    result = eq.OUT_get(%i)
 except Exception as e:
-        info = sys.exc_info()
-        s = traceback.format_tb(info[2])
-        print(str(e) + ' ... \\n' + ''.join(s))
-        sys.stdout.flush()
-        result = 'EQ_ABORT'
+    import sys, traceback
+    info = sys.exc_info()
+    s = traceback.format_tb(info[2])
+    print(str(e) + ' ... \\n' + ''.join(s))
+    sys.stdout.flush()
+    result = 'EQ_ABORT'
 """;
 
-(string result) EQ_get() {
-    result = python_persist(code_get, "result");
+(string result) EQ_get(int eq_type) {
+    result = python_persist(code_get % eq_type, "result");
 }
 
-string code_put = "import eq ; eq.init() ; eq.IN_put('%s')";
+// string code_put = "import eq ; eq.init() ; eq.IN_put('%s')";
 
-(void v) EQ_put(string data) {
-    python_persist(code_put % data) => v = propagate();
+string code_put = """
+import eq
+eq.init()
+try:
+    result = eq.IN_put(%i, '%s')
+except Exception as e:
+    import sys, traceback
+    info = sys.exc_info()
+    s = traceback.format_tb(info[2])
+    print(str(e) + ' ... \\n' + ''.join(s))
+    sys.stdout.flush()
+    result = 'EQ_ABORT'
+""";
+
+(void v) EQ_put(int eq_type, string data) {
+    python_persist(code_put % (eq_type, data)) => v = propagate();
 }
 
 // Local Variables:
