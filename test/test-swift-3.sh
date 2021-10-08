998e52eaa9686a6e51ff4598604afda59b504a63
Justin Wozniak <woz@anl.gov>
2021-10-08 09:56:02 -0500

    Check queues here

--- test/test-swift-3.sh
+++ test/test-swift-3.sh
@@ -7,6 +7,8 @@ THIS=$(   readlink --canonicalize $( dirname $0 ) )
 EQ_SQL=$( readlink --canonicalize $THIS/.. )
 export EQ_SQL
 
+$THIS/check-queues.sh
+
 export PYTHONPATH=$EQ_SQL/db:$EQ_SQL/python
 
 swift-t -I $EQ_SQL/swift $EQ_SQL/swift/loop.swift &
