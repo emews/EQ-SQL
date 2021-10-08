17145b7a7be2a7c6f782bca68f6d35d383701326
Justin Wozniak <woz@anl.gov>
2021-10-08 09:47:40 -0500

    Fix test

--- test/test-swift-2.sh
+++ test/test-swift-2.sh
@@ -10,8 +10,10 @@ export EQ_SQL
 export PYTHONPATH=$EQ_SQL/db:$EQ_SQL/python:$EQ_SQL/test
 
 source $EQ_SQL/db/db-settings.sh
+$EQ_SQL/test/check-queues.sh
 
-swift-t -I $EQ_SQL/swift $EQ_SQL/swift/loopj.swift &
+swift-t -I $EQ_SQL/swift $EQ_SQL/swift/loopj-type.swift &
+JOB=${!}
 python $THIS/test-swift-2-me.py
 
-wait
+wait $JOB
