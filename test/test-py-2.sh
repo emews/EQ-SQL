f9b292ff31512bc401c232b010dea01301d2905e
Justin Wozniak <woz@anl.gov>
2021-10-08 09:55:37 -0500

    Shorter call to script

--- test/test-py-2.sh
+++ test/test-py-2.sh
@@ -9,7 +9,7 @@ EQ_SQL=$( readlink --canonicalize $THIS/.. )
 export PYTHONPATH=$EQ_SQL/db:$EQ_SQL/python
 
 source $EQ_SQL/db/db-settings.sh
-$EQ_SQL/test/check-queues.sh
+$THIS/check-queues.sh
 
 which python3
 python3 $EQ_SQL/test/test-py-2-me.py &
