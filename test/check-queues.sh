828b4f2cc7d28a504ca81dc3aa13e8ad30bf2268
Justin Wozniak <woz@anl.gov>
2021-10-08 09:42:30 -0500

    New queue checker tool wrapper

---
+++ test/check-queues.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -eu
+
+# CHECK QUEUES SH
+# Check that the EMEWS Queues are empty before running a test
+
+THIS=$(   readlink --canonicalize $( dirname $0 ) )
+EQ_SQL=$( readlink --canonicalize $THIS/.. )
+export EQ_SQL
+
+export PYTHONPATH=$EQ_SQL/db
+python3 $THIS/check-queues.py
