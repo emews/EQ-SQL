76708ff17a13314b9bebe6fd2b1a1b231f0ce429
Justin Wozniak <woz@anl.gov>
2022-02-10 22:49:33 -0600

    Fix up test-py-1

--- test/test-py-1-me.py
+++ test/test-py-1-me.py
@@ -11,7 +11,8 @@ print("PY TEST 1 ME: START")
 eq.init()
 
 for i in range(0, 3):
-    eq.DB_submit(0, "{message:%i}" % i)
+    eq_id = eq.DB_submit(0, "{message:%i}" % i)
+    eq.OUT_put(0, eq_id)
 eq.DB_final()
 
 print("PY TEST 1 ME: STOP")
--- test/test-py-1-wf.py
+++ test/test-py-1-wf.py
@@ -9,12 +9,16 @@ print("PY TEST 1 WF: START")
 eq.init()
 
 while True:
-    tpl = eq.OUT_get(eq_type=0)
-    print("tuple: " + str(tpl))
-    eq_id, eq_type, msg = tpl
+    msg = eq.OUT_get(eq_type=0)
+    print("msg: " + str(msg))
     if msg == "EQ_FINAL":
         break
     if msg == "EQ_ABORT":
         break
+    eq_id = int(msg)
+    print("eq_id=%i" % eq_id)
+    params = eq.DB_json_out(eq_id)
+    print("params: " + str(params))
+
 
 print("PY TEST 1 WF: STOP")
