ca4481df2ca9c91eeb19b3dda9fb00229b89ad1b
Nick Collier <ncollier@anl.gov>
2022-02-25 15:42:00 -0500

    Added check for host

--- pools/swift/test/run_deap_me.sh
+++ pools/swift/test/run_deap_me.sh
@@ -9,7 +9,11 @@ export EQ_SQL
 
 export PYTHONPATH=$EQ_SQL/db:$EQ_SQL/python:$EQ_SQL/test
 
-export DB_HOST=beboplogin3.lcrc.anl.gov
+if [[ ${DB_HOST:-} == "" ]]
+  then
+    echo "Set DB_HOST!"
+    return 1
+fi
 source $EQ_SQL/db/db-settings.sh
 python $EQ_SQL/test/test-swift-2-me.py
 
