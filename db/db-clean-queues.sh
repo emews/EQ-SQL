2d27918dd2fbdb7025e2faa9eabf6da8d26a2a95
Eric Tatara <tatara@anl.gov>
2022-04-26 15:22:23 -0500

    shell script for deleting just the emews in.out queues

---
+++ db/db-clean-queues.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -eu
+
+# DB CLEAN QUEUES SH
+# Deletes the EMEWS queues IN and OUT!
+
+# Environment variables:
+# Set DB_CONFIRM=1   to retain    confirmations (default)
+# Set DB_CONFIRM=0   to shorten   confirmations
+# Set DB_CONFIRM=OFF to eliminate confirmations
+
+if (( ${#} != 0 ))
+then
+  echo "Too many arguments!"
+  exit 1
+fi
+
+THIS=$( readlink --canonicalize $( dirname $0 ) )
+source $THIS/db-settings.sh -v $*
+
+DB_DELAY=5
+if [[ ${DB_CONFIRM:-1} == 1 ]]
+then
+  echo
+  echo "Deleting all rows from EMEWS queue tables ... Enter to confirm ... Ctrl-C to cancel ..."
+  read _
+  echo "Deleting all rows from EMEWS queue tables ..."
+  echo
+  # ignore non-zero exit code for no input:
+  read -t $DB_DELAY _ || true
+else
+  echo "Deleting all rows from EMEWS queue tables ..."
+  if [[ ${DB_CONFIRM} == "OFF" ]]
+  then
+    DB_DELAY=0
+  else
+    DB_DELAY=1
+  fi
+  # ignore non-zero exit code for no input:
+  read -t $DB_DELAY _ || true
+fi
+echo
+
+sql <<EOF
+\set ON_ERROR_STOP on
+\dt
+select pg_sleep($DB_DELAY);
+
+delete from emews_queue_OUT;
+delete from emews_queue_IN;
+
+EOF
+
+echo "db-clean-queues.sh: OK"
