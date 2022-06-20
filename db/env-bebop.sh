7055b48c90d606463e33f484b98db69b38f27573
collier <collier@anl.gov>
2022-06-20 14:17:24 -0500

    Added dynamic port selection

--- db/env-bebop.sh
+++ db/env-bebop.sh
@@ -1,11 +1,12 @@
 module load readline/7.0-jalnd7f 
 
 export DB_HOST=${DB_HOST:-$(hostname --long)} # thetalogin4
-# The port serving the DB
-export DB_PORT=${DB_PORT:-11219}
+# Use python to find a free port
+PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
+export DB_PORT=${DB_PORT:-$PORT}
 # The user name to use for the DB
 export DB_USER=${DB_USER:-}
-export DB_DATA=/lcrc/project/EMEWS/bebop/db
+export DB_DATA=${DB_DATA:-/lcrc/project/EMEWS/bebop/db}
 export DB_MODE=ON
 
 export PATH=/lcrc/project/EMEWS/bebop/sfw/gcc-7.1.0/postgres-14.2/bin:$PATH
