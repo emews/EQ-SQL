f08d04eec04bd4d5f12902226740af1daf05f432
Nick Collier <ncollier@anl.gov>
2023-03-30 10:39:12 +0200

    Added test swift submission script

---
+++ python/test_data/test_swift_submit.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/env bash
+
+set -eu
+
+if [ "$#" -ne 2 ]; then
+  script_name=$(basename $0)
+  echo "Usage: ${script_name} exp_id cfg_file"
+  exit 1
+fi
+
+export TURBINE_LOG=0 TURBINE_DEBUG=0 ADLB_DEBUG=0
+# export TURBINE_STDOUT=out-%%r.txt
+export TURBINE_STDOUT=
+export ADLB_TRACE=0
+export EMEWS_PROJECT_ROOT=$( cd $( dirname $0 )/.. ; /bin/pwd )
+# source some utility functions used by EMEWS in this script                                                                                 
+# source "${EMEWS_PROJECT_ROOT}/swift-t/ext/emews_utils.sh"
+
+export EXPID=$1
+export TURBINE_OUTPUT=$EMEWS_PROJECT_ROOT/experiments/$EXPID
+# check_directory_exists
+
+CFG_FILE=$2
+source $CFG_FILE
+
+echo "--------------------------"
+# echo "WALLTIME:              $CFG_WALLTIME"
+echo "PROCS:                 $CFG_PROCS"
+echo "PPN:                   $CFG_PPN"
+echo "DB_HOST:               $CFG_DB_HOST"
+echo "DB_USER:               $CFG_DB_USER"
+echo "TASK_TYPE:             $CFG_TASK_TYPE"
+echo "--------------------------"
+
+export PROJECT=$CFG_PROJECT
+export QUEUE=$CFG_QUEUE
+export WALLTIME=$CFG_WALLTIME
+
+export PROCS=$CFG_PROCS
+export PPN=$CFG_PPN
+export TURBINE_JOBNAME="${EXPID}_job"
+
+swift-t -m slurm -i sys -E 'sleep(600) => trace("DONE");'
