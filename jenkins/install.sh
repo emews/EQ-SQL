5bed1bc65bca0ca9c5d108e1ab480990b0eda13d
Justin Wozniak <woz@anl.gov>
2023-08-16 13:15:48 -0500

    Install basically works, start on test

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -1,37 +1,45 @@
-#!/bin/bash
+#!/bin/zsh
 set -eu
 
 # JENKINS INSTALL
 # Test that we can install the EQ-SQL stack
 # Can be tested outside Jenkins, simply set
 # environment variable WORKSPACE to the EQ-SQL clone
+# Provide -m to skip the Miniconda (re-)installation
 
 renice --priority 19 --pid $$
 
-# The Miniconda we are working with:
-MINICONDA=Miniconda3-py39_23.3.1-0-Linux-x86_64.sh
+zparseopts -D -E m=M
 
-# Clean up prior runs
-# rm -fv $MINICONDA
-# rm -fr $WORKSPACE/sfw/Miniconda
+install-miniconda()
+{
+  # The Miniconda we are working with:
+  MINICONDA=Miniconda3-py39_23.3.1-0-Linux-x86_64.sh
 
-# (
-#   # Download and install both Minicondas:
-#   set -x
-#   wget --no-verbose https://repo.anaconda.com/miniconda/$MINICONDA
-#   bash $MINICONDA -b -p $WORKSPACE/sfw/Miniconda
-# )
+  # Clean up prior runs
+  rm -fv $MINICONDA
+  rm -fr $WORKSPACE/sfw/Miniconda
+
+  (
+    # Download and install both Minicondas:
+    set -x
+    wget  https://repo.anaconda.com/miniconda/$MINICONDA  # --no-verbose
+    bash $MINICONDA -b -p $WORKSPACE/sfw/Miniconda
+  )
+}
+
+if (( ${#M} == 0 )) install-miniconda
 
 PATH=$WORKSPACE/sfw/Miniconda/bin:$PATH
 
 set -x
 which conda
-conda install -c swift-t -c conda-forge swift-t
+conda install --yes -c swift-t -c conda-forge swift-t postgresql
 # conda install -c swift-t swift-t-r
-conda install postgres
 
-# Development pip install (pip install -e) eqsql python packages
+cd python
 pip install -e .
+cd -
 
 git clone https://github.com/emews/emews-project-creator.git
 cd emews-project-creator
---
+++ jenkins/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# TEST SH
+# Run this after ./install.sh
+
+emewscreator init_db
+
+# Test using this workflow:
+
+git clone https://github.com/emews/emews_examples
