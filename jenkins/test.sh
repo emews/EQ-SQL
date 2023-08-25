595210b1d0f6041e2bf86b0b6a31d6e094971cce
Justin Wozniak <woz@anl.gov>
2023-08-25 14:40:21 -0500

    Start on EMEWS test

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+set -eu
 
 # TEST SH
 # Run this after ./install.sh
 
+EMEWS_INSTALL=$( cd $WORKSPACE/../EMEWS-Install ; pwd -P )
+PATH=$EMEWS_INSTALL:$PATH
+
+set -x
+which emewscreator
+emewscreator --help
 emewscreator init_db
 
 # Test using this workflow:
