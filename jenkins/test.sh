680779c82f362e9aaff9644710896b7ed530c143
Justin Wozniak <woz@anl.gov>
2023-08-25 14:57:36 -0500

    Start/stop the DB

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -7,12 +7,17 @@ set -eu
 set -x
 EMEWS_INSTALL=$( cd $WORKSPACE/../EMEWS-Install ; pwd -P )
 PATH=$EMEWS_INSTALL/sfw/Miniconda/bin:$PATH
+PATH=$EMEWS_INSTALL/db:$PATH
 
 set -x
+DB=$WORKSPACE/DB
 which emewscreator
 emewscreator --help
-emewscreator init_db
+emewscreator init_db --db_path $DB
+
+db-start.sh $DB
+
+db-stop.sh $DB
 
 # Test using this workflow:
-
 git clone https://github.com/emews/emews_examples
